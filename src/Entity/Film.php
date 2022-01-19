eb6f751766ced45a0abc01bc26a0103d58c4bf8b
atlas Sanchez <jgsan360@gmail.com>
2022-01-19 13:42:40 +0100

    ajout de mon entité film

---
+++ src/Entity/Film.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilmRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FilmRepository::class)]
+class Film
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $nom;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $description;
+
+    #[ORM\Column(type: 'float')]
+    private $note;
+
+    #[ORM\Column(type: 'integer')]
+    private $nbre_votants;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getNote(): ?float
+    {
+        return $this->note;
+    }
+
+    public function setNote(float $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getNbreVotants(): ?int
+    {
+        return $this->nbre_votants;
+    }
+
+    public function setNbreVotants(int $nbre_votants): self
+    {
+        $this->nbre_votants = $nbre_votants;
+
+        return $this;
+    }
+}
